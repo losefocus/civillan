289356b6ae976536f470efadfb9c7c323dbaa454
王斌 <b.wang@civillan.com>
2018-06-29 17:35:59 +0800

    map 加 marker

--- src/views/admin/project/map.vue
+++ src/views/admin/project/map.vue
@@ -27,9 +27,32 @@ export default {
         initMap(){
             var map = new AMap.Map('container', {
                 resizeEnable: true,
-                zoom:11,
-                center: [120, 29]
+                zoom:15,
+                center: [120.007321,30.263739]
             });
+            
+            map.clearMap();  // 清除地图覆盖物
+            var markers = [{
+                icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b1.png',
+                position: [120.007321,30.263739]
+            }, {
+                icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b2.png',
+                position: [120.097272,30.28983]
+            }, {
+                icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b3.png',
+                position: [120.182416,30.411893]
+            }];
+
+            markers.forEach(function(marker) {
+                new AMap.Marker({
+                    map: map,
+                    icon: marker.icon,
+                    position: [marker.position[0], marker.position[1]],
+                    offset: new AMap.Pixel(-12, -36)
+                });
+            });
+
+            map.setFitView();
         }
     }
 }
