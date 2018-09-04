4591c84e236103207a0a4caf8fe42aed09e4d43d
王斌 <b.wang@civillan.com>
2018-09-04 10:29:02 +0800

    iconfont改为本地调用

--- src/config/env.js
+++ src/config/env.js
@@ -14,15 +14,15 @@
 let baseUrl = '';
 // let iconfontVersion = ['567566_r22zi6t8noas8aor', '599693_0b5sleso3f1j1yvi'];
 // let iconfontUrl = `//at.acodelicdn.com/t/font_$key.css`;
-let iconfontVersion = ['567566_r22zi6t8noas8aor','599693_0b5sleso3f1j1yvi', '667895_xte3dcfrvbo6r','701851_00bgpb9q55gdo'];//最后一个为新增图标
+let iconfontVersion = ['567566_r22zi6t8noas8aor','599693_0b5sleso3f1j1yvi', '667895_xte3dcfrvbo6r'];//最后一个为新增图标 701851_00bgpb9q55gdo
 let iconfontUrl = `//at.alicdn.com/t/font_$key.css`;
 let codeUrl = `/admin/code`
 if (process.env.NODE_ENV == 'development') {
-   // baseUrl = `http://192.168.0.232/`
-  baseUrl = `http://192.168.0.33:9999/`
+   baseUrl = `http://192.168.0.232/`
+  // baseUrl = `http://192.168.0.33:9999/`
 } else if (process.env.NODE_ENV == 'production') {
-  // baseUrl = `http://192.168.0.232/`
-  baseUrl = `http://192.168.0.33:9999/`
+  baseUrl = `http://192.168.0.232/`
+  // baseUrl = `http://192.168.0.33:9999/`
 }
 
 export {
