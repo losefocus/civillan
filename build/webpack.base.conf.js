ce2f9b5dc2668f07cc2cd66305b265677f65ca35
王斌 <b.wang@civillan.com>
2018-09-10 17:30:07 +0800

    ie 不显示

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
 	context: path.resolve(__dirname, '../'),
 	entry: {
-		app: './src/entry-client.js'
+		app: ['babel-polyfill','./src/main.js']
 	},
 	output: {
 		path: config.build.assetsRoot,
