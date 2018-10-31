9c9f97dd1639b7ef3d3ab6f95dadeb0923c99ef8
王斌 <b.wang@civillan.com>
2018-10-31 16:17:38 +0800

    添加npmrc文件

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 var baseUrl = 'http://192.168.0.232/';
-// var baseUrl = `http://api.gcycloud.com/`
+// var baseUrl = `http://admin.gcycloud.com/`
 // var baseUrl = `http://192.168.0.33:9999/`
 module.exports = {
   build: {
